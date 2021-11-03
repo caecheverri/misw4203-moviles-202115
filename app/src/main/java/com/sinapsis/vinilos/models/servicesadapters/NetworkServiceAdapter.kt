05fe7a722f562d49f518df0c4452553ec5213c49
caecheverri <92674910+caecheverri@users.noreply.github.com>
2021-11-03 14:32:34 -0500

    Feature/hu02 listados artistas (#5)
    * Configuracion librerias
    
    * Desarrollo hu02 listado de artistas
    
    * adcion archivos idea
    
    Co-authored-by: Carlos Echeverri <cecheverri@mihhgs.com>
    Co-authored-by: faropo <faropo@gmail.com>

---
+++ app/src/main/java/com/sinapsis/vinilos/models/servicesadapters/NetworkServiceAdapter.kt
@@ -0,0 +1,83 @@
+package com.sinapsis.vinilos.models.servicesadapters
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.sinapsis.vinilos.models.Artista
+import org.json.JSONArray
+import org.json.JSONObject
+
+/**
+ * Implementa el patrón Service Adapter para interactuar con el
+ * API de Vinilos
+ */
+class NetworkServiceAdapter constructor(context: Context) {
+    /**
+     * Crea la instancia singleton del adaptador
+     */
+    companion object{
+        const val BASE_URL= "https://back-vinyls-populated.herokuapp.com/"
+        var instance: NetworkServiceAdapter? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: NetworkServiceAdapter(context).also {
+                    instance = it
+                }
+            }
+    }
+
+    /**
+     * Crea la cola de Volley
+     */
+    private val requestQueue: RequestQueue by lazy {
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    /**
+     * Invoca el servicio del API que retorna todos los artistas
+     */
+    fun getArtistas(onComplete:(resp:List<Artista>)->Unit, onError: (error:VolleyError)->Unit){
+        requestQueue.add(getRequest("musicians",
+            { response ->
+                val resp = JSONArray(response)
+                val list = mutableListOf<Artista>()
+                for (i in 0 until resp.length()) {
+                    val item = resp.getJSONObject(i)
+                    list.add(i, Artista(
+                        artistaId = item.getInt("id"),
+                        nombre = item.getString("name"),
+                        imagen = item.getString("image")))
+                }
+                onComplete(list)
+            },
+            {
+                onError(it)
+            }))
+    }
+
+    /**
+     * Realiza una petición GET al API
+     */
+    private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+        return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+    }
+
+    /**
+     * Realiza una petición POST al API
+     */
+    private fun postRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
+    }
+
+    /**
+     * Realiza una petición PUT al API
+     */
+    private fun putRequest(path: String, body: JSONObject,  responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
+    }
+}
---
+++ app/src/main/java/com/sinapsis/vinilos/viewmodels/ArtistaViewModel.kt
@@ -0,0 +1,49 @@
+package com.sinapsis.vinilos.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.sinapsis.vinilos.models.Artista
+import com.sinapsis.vinilos.models.repositories.ArtistaRepository
+
+/**
+ * Implementa el ViewModel para las funcionalidades de Artista
+ */
+class ArtistaViewModel (application: Application) : AndroidViewModel(application) {
+    private val _artistas = MutableLiveData<List<Artista>>()
+    val artistas: LiveData<List<Artista>> get() = _artistas
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+    val eventNetworkError: LiveData<Boolean> get() = _eventNetworkError
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+    val isNetworkErrorShown: LiveData<Boolean> get() = _isNetworkErrorShown
+
+    private val albumRepository = ArtistaRepository(application)
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        albumRepository.getArtistas({list ->
+            _artistas.postValue(list)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ArtistaViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ArtistaViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
